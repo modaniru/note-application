036129800abb88ce8448416b6713129654af1973
Aleksei <mokshantsev.aleksei@yandex.ru>
2022-05-16 09:03:43 +0500

    note web app

--- src/main/java/com/modaniru/note/app/controller/NoteController.java
+++ src/main/java/com/modaniru/note/app/controller/NoteController.java
@@ -3,51 +3,28 @@ package com.modaniru.note.app.controller;
 
 import com.modaniru.note.app.entity.*;
 import com.modaniru.note.app.exception_handling.NoSuchNoteException;
+import com.modaniru.note.app.service.NotePreviewService;
 import com.modaniru.note.app.service.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@RestController
-@RequestMapping("/api")
+@Controller
+@RequestMapping("/note")
 public class NoteController {
 
     @Autowired
-    private NoteService noteService;
+    NoteService noteService;
 
-    @CrossOrigin(origins = "http://127.0.0.1:5500")
-    @GetMapping(value = "/note", produces = "application/json")
-    public List<Note> getAllNotes(){
-        return noteService.getAll();
-    }
-
-    @CrossOrigin(origins = "http://127.0.0.1:5500")
-    @GetMapping(value = "/note/{id}",  produces = "application/json")
-    public Note getNote(@PathVariable int id){
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public String getNote(@PathVariable("id") int id, Model model){
         Note note = noteService.getNote(id);
-        if(note==null){
-            throw new NoSuchNoteException("A note was not found");
-        }
-        return note;
-    }
-
-    @CrossOrigin(origins = "http://127.0.0.1:5500")
-    @PostMapping(value = "/note", produces = "application/json")
-    public void saveNote(@RequestBody Note note){
-        noteService.save(note);
-    }
-
-    @CrossOrigin(origins = "http://127.0.0.1:5500")
-    @DeleteMapping(value = "/note{id}", produces = "application/json")
-    public String deleteNote(@PathVariable int id){
-        Note note = noteService.getNote(id);
-        if(note==null){
-            throw new NoSuchNoteException("A note was not found");
-        }
-        noteService.delete(id);
-        return "A note with id: " + id + " was deleted";
+        model.addAttribute("note", note);
+        return "note";
     }
 }
